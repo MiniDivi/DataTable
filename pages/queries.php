fb687f388165071b1e6bbcce9e38694427657bc6
MiniDivi <alessandro.divincenzo77@gmail.com>
2022-05-24 16:46:10 +0200

    modifiche

--- pages/queries.php
+++ pages/queries.php
@@ -12,23 +12,6 @@ function postRequest($first, $lenght, $order){
     return $rows;
 }
 
-// function postRequestSearch($first, $lenght, $order, $search){
-//     require('connection.php');
-//     // $query = "SELECT * FROM employees
-//     //                  WHERE id LIKE $search OR birth_date LIKE $search OR
-//     //                         first_name LIKE $search OR last_name LIKE $search OR
-//     //                         gender LIKE $search OR hire_date LIKE $search                            
-//     //                  ORDER BY id $order LIMIT $first, $lenght";
-//     $query = "SELECT * FROM employees WHERE first_name = '".$search."'";
-//     if($result = $mysqli->query($query)){
-//     $rows = array();
-//     while ($row= $result-> fetch_assoc()) {
-//         $rows[]=$row;
-//     }
-// }
-//     return $rows;
-// }
-
 function putRequest($id, $birth_date, $first_name, $last_name, $gender, $hire_date){
     require('connection.php');
     $query = "UPDATE employees
